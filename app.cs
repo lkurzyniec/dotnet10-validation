43454b1ae1061fce75375b7cd6ab5789a4512f01
Łukasz Kurzyniec <lkurzyniec@gmail.com>
2025-11-23 02:14:41 +0100

    added working and not working example

---
+++ app.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddValidation();
+builder.Services.AddOpenApi();
+
+using var app = builder.Build();
+
+app.MapOpenApi();
+app.MapScalarApiReference("api-doc");
+
+app.MapPost("/working", (WorkingValidationDto dto) => TypedResults.Ok(dto));
+app.MapPost("/not-working", (NotWorkingValidationDto dto) => TypedResults.Ok(dto));
+
+await app.RunAsync();
+
+public record WorkingValidationDto(int? Id, [property: Required] string Title);
+
+public record struct NotWorkingValidationDto(int? Id, [property: Required] string Title);
